f393f26cf405303b491e6ef4aea823be9b70ac8d
Chandrima <chandrima.mj@gmail.com>
2021-03-26 09:44:26 +0400

    splash

--- app/src/main/java/com/example/s_martapp/SplashActivity.java
+++ app/src/main/java/com/example/s_martapp/SplashActivity.java
@@ -21,6 +21,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
 
         firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
 
